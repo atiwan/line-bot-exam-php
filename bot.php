8c4b0e7c3a260b932e639d94c2fd59c5f1cbc3db
atiwan <49089182+atiwan@users.noreply.github.com>
2019-03-30 15:02:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,6 +15,8 @@
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        $arrayPostData['messages'][1]['packageId'] = "2";
+        $arrayPostData['messages'][1]['stickerId'] = "34"; 
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Sticker"
